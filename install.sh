fb90047e9303a0b9419d63357d208710d0b0042a
graypawn <choi.pawn@gmail.com>
2016-03-12 22:15:10 +0900

    각종 설정 파일을 심볼릭 링크하는 스크립트

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# 스크립트의 절대 경로를 얻는다.
+EXEC_FILE="$0"
+BASE_NAME=`basename "$EXEC_FILE"`
+if [ "$EXEC_FILE" = "./$BASE_NAME" ] || [ "$EXEC_FILE" = "$BASE_NAME" ]; then
+        FULL_PATH=`pwd`
+else
+        FULL_PATH=`echo "$EXEC_FILE" | sed 's/'"${BASE_NAME}"'$//'`
+        cd "$FULL_PATH"                 > /dev/null 2>&1
+        FULL_PATH=`pwd`
+fi
+
+# 각종 폴더를 심볼릭 링크한다.
+ln -s $FULL_PATH/.lein ~/
+
+mkdir ~/.config/ 2>/dev/null
+ln -s $FULL_PATH/fcitx ~/.config/
+ln -s $FULL_PATH/xfce4 ~/.config/
